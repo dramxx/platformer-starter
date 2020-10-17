1771a233217b8a1ff4ba5cc13bdba840128c8340
dramxx <drahoslav.madar@gmail.com>
2020-10-17 22:07:14 +0200

    adding platforms

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,12 +1,19 @@
 import Phaser from "phaser";
 
 export default class Game extends Phaser.Scene {
+  private platforms?: Phaser.Physics.Arcade.StaticGroup;
+
   constructor() {
     super("game");
   }
 
   create() {
     this.add.image(400, 300, "background");
-    this.add.image(400, 300, "star");
+
+    this.platforms = this.physics.add.staticGroup();
+    this.platforms.create(400, 568, "platform").setScale(2).refreshBody();
+    this.platforms.create(600, 400, "platform");
+    this.platforms.create(50, 250, "platform");
+    this.platforms.create(750, 220, "platform");
   }
 }
